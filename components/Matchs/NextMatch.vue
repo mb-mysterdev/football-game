11184d5c4c51ba520ce89d0f20908a645441ff26
Bouhlel Mohamed <mysterdev@MBP-de-Bouhlel.home>
2021-10-18 21:15:32 +0200

    delete unused mutations

--- components/Matchs/NextMatch.vue
+++ components/Matchs/NextMatch.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="pt-5 mt-5 d-flex justify-content-center">
-    <b-row v-for="(match,key) in matches" :key="key" class="mr-5">
+    <b-row class="mr-5">
       <div
-        v-if="match.matchStatus === 'inProgress' && !loading"
+        v-if="!loading && getNextMatch !== null"
       >
         <b-row>
           <b-col>
             <div class="card bg-transparent" style="width: 15rem;">
               <div style="height:14rem;width:14rem" class="d-flex justify-content-center">
-                <img :src="getTeam(match.team1).logo" class="card-img-top" style="height:11rem;width:11rem">
+                <img :src="user.team.team_logo" class="card-img-top" style="height:11rem;width:11rem">
+              </div>
+              <div v-if="showResult !== null" class="d-flex justify-content-center">
+                {{ showResult.score_team_user }}
               </div>
               <div class="card-body d-flex justify-content-center">
                 <h5 class="card-title text-light">
-                  {{ getTeam(match.team1).name }}
+                  {{ user.team.team_name }}
                 </h5>
               </div>
               <div class="d-flex justify-content-center">
@@ -26,7 +29,7 @@
           </b-col>
           <b-col class="white">
             <h3>Next Match</h3>
-            <h3>{{ user.team.team_division.division_name }} / S{{ user.week }}</h3>
+            <h3>{{ user.team.team_division.division_name }} / S 1</h3>
             <div style="font-size:120px" :style="'color:'+user.team.team_division.division_second_color ">
               VS
             </div>
@@ -34,11 +37,14 @@
           <b-col>
             <div class="card bg-transparent" style="width: 15rem;">
               <div style="height:14rem;width:14rem" class="d-flex justify-content-center">
-                <img :src="getTeam(match.team2).logo" class="card-img-top" style="height:11rem;width:11rem">
+                <img :src="getNextMatch.team_logo" class="card-img-top" style="height:11rem;width:11rem">
               </div>
-              <div class="card-body">
+              <div v-if="showResult !== null" class="d-flex justify-content-center">
+                {{ showResult.score_team }}
+              </div>
+              <div class="card-body d-flex justify-content-center">
                 <h5 class="card-title text-light">
-                  {{ getTeam(match.team2).name }}
+                  {{ getNextMatch.team_name }}
                 </h5>
               </div>
               <div class="d-flex justify-content-center">
@@ -52,12 +58,15 @@
           </b-col>
         </b-row>
         <div class="d-flex justify-content-center">
-          <b-button @click="nextMatch(match,key)">
-            NEXT MATCH
+          <b-button v-if="showResult === null" @click="playMatch(getNextMatch)">
+            Play Match
+          </b-button>
+          <b-button v-else @click="showResult = null">
+            Next Match
           </b-button>
         </div>
       </div>
-      <div v-else-if="match.matchStatus === 'inProgress' && loading">
+      <div v-else-if="loading">
         <b-skeleton-img height="20rem" width="25rem" />
       </div>
     </b-row>
@@ -79,31 +88,34 @@ export default {
   data () {
     return {
       loading: false,
-      matches: [
-        { team1: 1, team2: 2, matchStatus: 'inProgress' },
-        { team1: 2, team2: 1, matchStatus: 'next' }
-      ],
-      teams: [
-        { id: 1, name: 'Paris Saint-Germain', logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/1200px-Paris_Saint-Germain_Logo.svg.png' },
-        { id: 2, name: 'Olympique lyonnais', logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/e2/Olympique_lyonnais_%28logo%29.svg/1200px-Olympique_lyonnais_%28logo%29.svg.png' },
-        { id: 2, name: 'Olympique lyonnais', logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/e2/Olympique_lyonnais_%28logo%29.svg/1200px-Olympique_lyonnais_%28logo%29.svg.png' }
-      ]
+      localUser: {},
+      showResult: null
     }
   },
+  computed: {
+    getNextMatch () {
+      if (this.showResult !== null) {
+        return this.localUser.matches.find(match => match.team_name === this.showResult.team_name)
+      }
+      if (this.localUser.matches !== undefined) {
+        return this.localUser.matches.find(match => match.score_team_user === null)
+      }
+      return null
+    }
+  },
+  mounted () {
+    this.localUser = JSON.parse(JSON.stringify(this.user))
+  },
   methods: {
-    getTeam (matchTeam) {
-      return this.teams.find(element => element.id === matchTeam)
-    },
-    nextMatch (match, key) {
-      setTimeout(function () {
-        this.loading = true
-
-        match.matchStatus = 'done'
-        this.matches[key + 1].matchStatus = 'inProgress'
-      }.bind(this), 1500)
-      setTimeout(function () {
-        this.loading = false
-      }.bind(this), 3000)
+    async playMatch () {
+      this.loading = true
+      this.localUser.matches.find(match => match.team_name === this.getNextMatch.team_name)
+      const foundIndex = this.localUser.matches.findIndex(match => match.team_name === this.getNextMatch.team_name)
+      this.localUser.matches[foundIndex].score_team_user = Math.round(Math.random() * 6)
+      this.localUser.matches[foundIndex].score_team = Math.round(Math.random() * 5)
+      await new Promise(resolve => setTimeout(resolve, 500))
+      this.loading = false
+      this.showResult = this.localUser.matches[foundIndex]
     }
   }
 }
--- store/user.js
+++ store/user.js
@@ -7,7 +7,6 @@ export const state = () => ({
     levelPoint: 755,
     popularity: 15,
     year: 2021,
-    week: 1,
     team: {
       team_name: 'PSG',
       team_logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/2048px-Paris_Saint-Germain_Logo.svg.png',
@@ -25,7 +24,81 @@ export const state = () => ({
         division_second_color: '#cefb05',
         division_background: ''
       }
-    }
+    },
+    matches: [
+      {
+        team_name: 'Rennes',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Stade_Rennais_FC.svg/1200px-Stade_Rennais_FC.svg.png',
+        team_last_match: [1, 0, 2],
+        team_local: 0,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'Olympique Lyonnais',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/e2/Olympique_lyonnais_%28logo%29.svg/1200px-Olympique_lyonnais_%28logo%29.svg.png',
+        team_last_match: [1, 0, 2],
+        team_local: 1,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'Olympique de Marseille',
+        team_power: 75,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/fr/archive/4/43/20200407190016%21Logo_Olympique_de_Marseille.svg',
+        team_last_match: [1, 2, 2],
+        team_local: 0,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'AS Monaco',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/5/58/Logo_AS_Monaco_FC_-_2021.svg/langfr-130px-Logo_AS_Monaco_FC_-_2021.svg.png',
+        team_last_match: [1, 0, 2],
+        team_local: 1,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'Stade de Reims',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/0/00/Logo_Stade_de_Reims_2020.svg/1200px-Logo_Stade_de_Reims_2020.svg.png',
+        team_last_match: [1, 0, 2],
+        team_local: 0,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'AS Saint Etienne',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/AS_Saint-%C3%89tienne_logo.png',
+        team_last_match: [1, 0, 2],
+        team_local: 1,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'LOSC Lille',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/6/62/Logo_LOSC_Lille_2018.svg/1200px-Logo_LOSC_Lille_2018.svg.png',
+        team_last_match: [1, 0, 2],
+        team_local: 0,
+        score_team: null,
+        score_team_user: null
+      },
+      {
+        team_name: 'FC Nantes',
+        team_power: 65,
+        team_logo: 'https://upload.wikimedia.org/wikipedia/commons/6/68/FC-Nantes-blason-rvb.png',
+        team_last_match: [1, 0, 2],
+        team_local: 1,
+        score_team: null,
+        score_team_user: null
+      }
+    ]
   }
 })
 
@@ -38,5 +111,11 @@ export const getters = {
   },
   getDivision: (state) => {
     return state.user.division
+  }
+}
+
+export const mutations = {
+  update (state, payload) {
+    state.user = payload
   }
 }
